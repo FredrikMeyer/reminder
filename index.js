0771052a6fdb11a054eddedcb3fba8722f53470c
Fredrik Meyer <hrmeyer@gmail.com>
2020-03-01 15:51:43 +0100

    Allow for using alert box.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { spawn } = require('child_process');
 
 program.option('-m, --minutes <minutes>', 'Remind in x minutes');
 program.option('-h, --hours <hours>', 'Remind in x hours');
+program.option('-a, --alert', 'Use alert instead of notification.')
 program.parse(process.argv);
 
 const message = program.args.join(" ")
@@ -15,16 +16,16 @@ if (program.minutes) {
     const { minutes } = program.opts()
     const parsedMinutes = parseFloat(minutes)
     console.log(`Reminding you in ${parsedMinutes} minute(s).`)
-    spawnReminder(parsedMinutes, message)
+    spawnReminder(parsedMinutes, message, program.alert)
 } else if (program.hours) {
     const { hours } = program.opts()
     const parsedHours = parseFloat(hours)
     console.log(`Reminding you in ${parsedHours} hour(s).`)
-    spawnReminder(hours * 60, message)
+    spawnReminder(hours * 60, message, program.alert)
 }
 
-function spawnReminder(minutes, message) {
-    spawn('./notifyIn.js', [JSON.stringify({minutes, message: message})], {
+function spawnReminder(minutes, message, useAlert) {
+    spawn('./notifyIn.js', [JSON.stringify({minutes, message: message, Boolean(useAlert)})], {
         stdio: 'inherit',
         detached: true
     }).unref()
--- notifyIn.js
+++ notifyIn.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
 
 const notifier = require('node-notifier');
+const { exec } = require('child_process');
 
-const {minutes, message} = JSON.parse(process.argv[2])
+const {minutes, message, useAlert} = JSON.parse(process.argv[2])
 
 function notifyIn(minutes, message) {
-    setTimeout(() => {
-        notifier.notify({
-            sound: true,
-            wait: true,
-            title: 'Reminder',
-            message
-        });
-        console.log("Reminding done.")
-    }, minutes * 60 * 1000)
+    if (useAlert) {
+        exec(`/usr/bin/osascript -e 'display alert "Reminder" message "${message}"'`)
+    } else {
+        setTimeout(() => {
+            notifier.notify({
+                sound: true,
+                wait: true,
+                title: 'Reminder',
+                message
+            });
+            console.log("Reminding done.")
+        }, minutes * 60 * 1000)
+    }
 }
 
 notifyIn(minutes, message);
